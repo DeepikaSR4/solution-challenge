25b93b8e91dc29cfcbc85c7b47a1d07658cb51d2
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2023-03-31 21:13:31 +0530

    edited

--- firebase.js
+++ firebase.js
@@ -19,10 +19,35 @@ loginForm.addEventListener('submit', e => {
 	auth.signInWithEmailAndPassword(email, password)
 		.then(() => {
 			// Redirect the user to the home page or do something else
-			window.location.href = 'home.html';
+			window.location.href = 'index.html';
 		})
 		.catch(error => {
 			// Display an error message if the authentication fails
 			errorMessage.textContent = error.message;
+		});
+});
+
+const authen = firebase.auth();
+
+// Get a reference to the signup form elements
+const signupForm = document.querySelector('form');
+const emailidInput = document.querySelector('#email');
+const passwordasInput = document.querySelector('#password');
+const errorsMessage = document.querySelector('#errorMessage');
+
+// Add an event listener to the signup form to handle form submission
+signupForm.addEventListener('submit', e => {
+	e.preventDefault(); // Prevent the form from submitting
+	const email = emailidInput.value;
+	const password = passwordasInput.value;
+	// Call the Firebase createUserWithEmailAndPassword method to create a new user
+	auth.createUserWithEmailAndPassword(email, password)
+		.then(() => {
+			// Redirect the user to the home page or do something else
+			window.location.href = 'home.html';
+		})
+		.catch(error => {
+			// Display an error message if the signup fails
+			errorsMessage.textContent = error.message;
 		});
 });
